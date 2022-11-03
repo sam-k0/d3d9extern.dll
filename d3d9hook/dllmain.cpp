525ec071fd37a302ac0862479f0ddef94f44a7d3
Samuel Koob <56673835+sam-k0@users.noreply.github.com>
2022-11-03 18:31:09 +0900

    Working basic now
    todo: set device as glob var

--- d3d9hook/dllmain.cpp
+++ d3d9hook/dllmain.cpp
@@ -5,24 +5,55 @@
 #include <d3d9.h>
 #include <d3dx9.h>
 #include <stdio.h>
-
+#include <string>
+#include "gms.h"
+#include <iostream>
 #pragma comment(lib, "d3d9.lib")
 #pragma comment(lib, "d3dx9.lib")
 
+LPD3DXFONT pFont;
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+//int text;
+
+VOID WriteText(LPDIRECT3DDEVICE9 pDevice, INT x, INT y, DWORD color, CHAR* text)
 {
-    switch (ul_reason_for_call)
+    RECT rect;
+    SetRect(&rect, x, y, x, y);
+    pFont->DrawTextA(NULL, text, -1, &rect, DT_NOCLIP | DT_LEFT, color);
+}
+
+// Maybe theres a more efficient way to get the handle other than passing it every frame... 
+// AFAIK the device handle only changes on window change, similar as to surfaces??
+gmx gmbool drawTo(stringToDLL handle, stringToDLL text, gmint dx, gmint dy)
+{
+    LPDIRECT3DDEVICE9 pDevice = (LPDIRECT3DDEVICE9)handle;
+
+    if (pFont)
     {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
+        pFont->Release();
+        pFont = NULL;
+    }
+    if (!pFont)
+    {
+        D3DXCreateFontA(pDevice, 14, 0, FW_BOLD, 1, 0, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, (LPCSTR)"Arial", &pFont);
+    }
+    // (CHAR*)"My Private Hook"
+    WriteText(pDevice, (INT)dx, (INT)80, D3DCOLOR_ARGB(255, 255, 000, 000),gmu::string_to_charptr(gmu::constcharptr_to_string(text)));      
+    
+    return gmtrue;
+}
+
+
+// DLLENTRY
+BOOL WINAPI DllMain(HINSTANCE hModule, DWORD dwReason, LPVOID lpvReserved)
+{
+    if (dwReason == DLL_PROCESS_ATTACH)
+    {
+        std::cout << "D3DXHook attached to process!" << std::endl;
     }
     return TRUE;
 }
+
+
+
 
