f8f78fdb473e352a6d99d7e4fb18540c326c2d4b
Samuel Koob <56673835+sam-k0@users.noreply.github.com>
2022-11-14 23:19:52 +0900

    Update shadercompile.h

--- d3d9hook/shadercompile.h
+++ d3d9hook/shadercompile.h
@@ -1,3 +1,20 @@
 #pragma once
 
 // this will handle shader compiling in the future
+#include <d3d9.h>
+#include <d3dx9.h>
+#include <string>
+void shadercompile(LPCSTR filename, LPCSTR funcName, LPCSTR profile, LPD3DXBUFFER* outBuffer, LPD3DXBUFFER* errBuffer)
+{
+	D3DXCompileShaderFromFileA(
+		filename,
+		NULL,
+		NULL,
+		funcName,
+		profile,
+		D3DXSHADER_AVOID_FLOW_CONTROL,
+		outBuffer,
+		errBuffer,
+		NULL
+	);
+}
