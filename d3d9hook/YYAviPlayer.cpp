f07c1bed9e537b4c72d5c3741c6b4b2d99bcfd5f
Samuel Koob <56673835+sam-k0@users.noreply.github.com>
2023-01-23 00:48:53 +0900

    add janky ass avi player

---
+++ d3d9hook/YYAviPlayer.cpp
@@ -0,0 +1,139 @@
+#include "pch.h"
+#include "YYAviPlayer.h"
+
+void CAviTexture::Open(LPDIRECT3DDEVICE9 pDevice, LPCSTR szFile)
+{
+	// Opens The AVIFile Library
+	AVIFileInit();
+	// Opens The AVI Stream
+	AVIStreamOpenFromFileA(&m_pavi, szFile, streamtypeVIDEO, 0, OF_READ, NULL); 
+	// Get stream information  
+	AVIStreamInfo(m_pavi, &m_si, sizeof(m_si)); 
+	// Prepare to decompress video frames 
+	m_pgf = AVIStreamGetFrameOpen(m_pavi, NULL); 
+	// Get the width and height 
+	m_width = m_si.rcFrame.right - m_si.rcFrame.left;  m_height = m_si.rcFrame.bottom - m_si.rcFrame.top;  
+	// Get the last frame of the stream 
+	m_lastFrame = AVIStreamLength(m_pavi);  
+	// Calculate rough milliseconds per frame
+	m_mpf = AVIStreamSampleToTime(m_pavi, 1);  
+	m_bmih.biSize        = sizeof (BITMAPINFOHEADER); 
+	// Size of the BitmapInfoHeader 
+	m_bmih.biPlanes      = 1;                         
+	// Bitplanes  
+	m_bmih.biBitCount    = 32;                        
+	// Bits Format  
+	m_bmih.biWidth       = 512; 
+	// Width
+	m_bmih.biHeight      = -512;  
+	// Height (Invert) 
+	m_bmih.biCompression = BI_RGB;        
+	// Requested Mode = RGB 
+	// Create a device context 
+	m_hDC = CreateCompatibleDC(0);  
+	// Create a DIB  
+	m_hBitmap = CreateDIBSection(m_hDC, (BITMAPINFO*)(&m_bmih), DIB_RGB_COLORS, (void**)(&m_pData), NULL, NULL);  
+	SelectObject(m_hDC, m_hBitmap); 
+	// Select the bitmap into the device context  
+	// Enable Dib Drawing  
+	m_hdd = DrawDibOpen();  
+	// Create the D3D texture
+	if( FAILED( pDevice->CreateTexture(512, 512, 0, D3DUSAGE_AUTOGENMIPMAP, D3DFMT_X8R8G8B8, D3DPOOL_MANAGED, &m_pTexture, NULL) ) )
+	    m_pTexture = NULL;  
+	// Zero data
+	m_next  = 0; 
+	m_frame = 0;
+}
+
+void CAviTexture::Close()
+{  // Release the D3D texture 
+	if( m_pTexture )  
+	{   
+		m_pTexture->Release();  
+		m_pTexture = NULL; 
+	}  
+	DrawDibClose(m_hdd); 
+	// Disable Dib Drawing 
+	DeleteObject(m_hBitmap);
+	// Delete the DIB object 
+	AVIStreamGetFrameClose(m_pgf); 
+	// Deallocate the GetFrame resources  
+	AVIStreamRelease(m_pavi);  
+	// Release the stream  
+	AVIFileExit();                
+	// Exit the AVIFile Library
+}
+
+void CAviTexture::Update(float frameTime){ 
+	LPBITMAPINFOHEADER lpbi;  
+	// Increase next based on frame time (milliseconds) 
+	m_next += static_cast<int>(frameTime);	m_frame = m_next/m_mpf; 
+	// Calculate the current frame
+	// If the video is complete  
+	if( m_frame >= m_lastFrame ) 
+	{  
+		m_frame = 0; // Reset the frame back to zero (start of video) 
+		m_next = 0;  // Reset the animation timer  
+	}  
+	// Get data from the AVI Stream  
+	lpbi = (LPBITMAPINFOHEADER)AVIStreamGetFrame(m_pgf, m_frame);
+	// Get a pointer to the data returned by AVIStreamGetFrame 
+	DWORD* pAviData = (DWORD *)lpbi + lpbi->biSize + lpbi->biClrUsed * sizeof(RGBQUAD);  
+	// Convert data to requested bitmap format 
+	DrawDibDraw(m_hdd, m_hDC, 0, 0, 512, 512, lpbi, pAviData, 0, 0, m_width, m_height, 0);   
+	// Lock the texture and get a pointer to the destination surface
+	D3DLOCKED_RECT rect;  m_pTexture->LockRect(0, &rect, NULL, 0);
+	// Get DWORD pointers to dest and src data 
+	DWORD* pDestData = (DWORD*)rect.pBits;  DWORD* pSrcData  = (DWORD*)m_pData; 
+	// Copy bitmap data to texture  
+	for( int y = 0 ; y < 512 ; y++ )  
+	{	   
+		// Copy this row of pixels 
+		memcpy( pDestData, pSrcData, 512 * sizeof( DWORD ) );   
+		// Skip past row of pixels
+		pDestData += ( rect.Pitch / 4 );    pSrcData += 512; 
+	} 
+	// Unlock the texture buffer  
+	m_pTexture->UnlockRect(0);
+}
+
+void CAviTexture::SetTexture(LPDIRECT3DDEVICE9 pDevice)
+{
+	pDevice->SetTexture(0, m_pTexture);
+}
+
+bool CAviTexture::Draw(LPDIRECT3DDEVICE9 pDevice, float x, float y, float width, float height)
+{
+	aviVertex vertices[] = {  
+		{       
+			x, y+height, 0.5f, 1.0f, 0.0f, 1.0f
+		},  
+		{   
+			x,y, 0.5f, 1.0f, 0.0f, 0.0f 
+		},   
+		{ 
+			x+width, y+height, 0.5f, 1.0f, 1.0f, 1.0f 
+		},    
+		{
+			x+width,        y, 0.5f, 1.0f, 1.0f, 0.0f 
+		} 
+	};
+	
+	pDevice->SetTexture(0, m_pTexture);
+	pDevice->SetFVF(D3DFVF_AVIVERTEX); 
+	pDevice->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, &vertices, sizeof(aviVertex));
+	
+	return true;
+}
+
+bool CAviTexture::Draw(LPD3DXSPRITE pSprite, float x, float y)
+{
+	//	if (SUCCEEDED(
+//	pSprite->Draw(m_pTexture, NULL, &D3DXVECTOR3(0, 0, 0), &D3DXVECTOR3(x, y, 0), D3DCOLOR_RGBA(255, 255, 255, 255));
+		//))
+	/* {
+			return true;
+		}
+		return false;		*/
+		return true;
+}
---
+++ d3d9hook/YYAviPlayer.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <Vfw.h>
+#include <string>
+#include <iostream>
+#include <d3d9.h>
+#include <d3d9helper.h>
+#include <d3dx9core.h>
+
+struct aviVertex{  float x, y, z, rhw;  float u, v;};
+#define D3DFVF_AVIVERTEX (D3DFVF_XYZRHW|D3DFVF_TEX1)
+
+class CAviTexture{
+
+	protected:  PAVISTREAM    m_pavi;
+    AVISTREAMINFO m_si;
+    PGETFRAME     m_pgf;
+    int m_width, m_height; 
+    long m_lastFrame;  
+    int m_mpf;  
+    BITMAPINFOHEADER m_bmih; 
+    HDC m_hDC;  
+    HBITMAP m_hBitmap; 
+    unsigned short* m_pData; 
+    HDRAWDIB m_hdd;  
+    LPDIRECT3DTEXTURE9 m_pTexture;
+    int m_next; 
+    int m_frame;
+
+    public:  
+        LPDIRECT3DTEXTURE9 GetTexture() { return m_pTexture; } 
+        
+        void Open(LPDIRECT3DDEVICE9 pDevice, LPCSTR szFile);
+        void Close();  
+        void Update(float frameTime); 
+        void SetTexture(LPDIRECT3DDEVICE9 pDevice); 
+        bool Draw(LPDIRECT3DDEVICE9 pDevice, float x, float y, float width, float height);
+        bool Draw(LPD3DXSPRITE pSprite, float x, float y);
+};
+
